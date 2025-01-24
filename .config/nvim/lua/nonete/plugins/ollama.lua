ed844db5a2710ebaeabbb275765e11e989dc00ce
Nonetss <amorenolopezbarajas@gmail.com>
2025-01-24 15:18:07 +0100

    Introducimos la nueva configuracion para el copilot

--- .config/nvim/lua/nonete/plugins/ollama.lua
+++ .config/nvim/lua/nonete/plugins/ollama.lua
@@ -1,14 +1,15 @@
 return {
-	"Nonetss/gen.nvim",
+	"Nonetss/gen-ollama.nvim",
 	opts = {
 		model = "deepseek-coder-v2:latest", -- El modelo predeterminado a utilizar.
+		thinking_model = "deepseek-r1:14b", -- Modelo por defecto para :TGen
 		quit_map = "q", -- establecer el mapa de tecla para cerrar la ventana de respuesta
 		retry_map = "<c-r>", -- establecer el mapa de tecla para reenviar el prompt actual
 		accept_map = "<c-cr>", -- establecer el mapa de tecla para reemplazar la selección anterior con el último resultado
 		host = "localhost", -- El host que ejecuta el servicio Ollama.
 		port = "11434", -- El puerto en el que el servicio Ollama está escuchando.
 		display_mode = "split", -- El modo de visualización. Puede ser "float" o "split" o "horizontal-split".
-		show_prompt = false, -- Muestra el prompt enviado a Ollama. Puede ser true (3 líneas) o "full".
+		show_prompt = true, -- Muestra el prompt enviado a Ollama. Puede ser true (3 líneas) o "full".
 		show_model = false, -- Muestra qué modelo estás utilizando al principio de tu sesión de chat.
 		no_auto_close = false, -- Nunca se cierra automáticamente la ventana.
 		file = false, -- Escribe la carga útil en un archivo temporal para mantener el comando corto.
@@ -35,4 +36,5 @@ return {
 		debug = false, -- Imprime errores y el comando que se ejecuta.
 	},
 	vim.keymap.set({ "n", "v" }, "<leader>ol", ":Gen<CR>"),
+	vim.keymap.set({ "n", "v" }, "<leader>ot", ":TGen<CR>"),
 }

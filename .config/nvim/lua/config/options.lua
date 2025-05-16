a821b16c41df755ca941ba2450f50a8459a0696d
Nonetss <amorenolopezbarajas@gmail.com>
2025-05-16 21:49:42 +0200

    add plugins vim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,16 @@
 vim.opt.spelllang = "es" -- Establecer el idioma del corrector (español)
 vim.opt.spell = true -- Habilitar el corrector ortográfico
 vim.opt.complete:append("kspell") -- Habilitar autocompletado basado en palabras detectadas por el corrector
+
+-- Vimtex
+-- Activa la detección de archivos, plugins e indentación
+vim.cmd("filetype plugin indent on")
+
+-- Activa la sintaxis
+vim.cmd("syntax enable")
+
+-- Configura el visor de PDFs
+vim.g.vimtex_view_method = "zathura"
+
+-- Configura el compilador (cambia latexmk por latexrun)
+vim.g.vimtex_compiler_method = "pdflatex"
